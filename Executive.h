231559360415005eb9e98510e9a63cb3882d1631
Ryan Kass <r954k200@ku.edu>
2017-09-12 13:25:36 -0500

    Added Executive.h outline

---
+++ Executive.h
@@ -0,0 +1,43 @@
+/**
+*	@file Executive.h
+*	@author Ferocious Hammerheads
+*	@date
+*	@brief A header file for Executive
+*/
+
+#ifndef EXECUTIVE_H
+#define EXECUTIVE_H
+
+#include <iostream>
+//more includes when files added
+
+class Executive
+{
+	public:
+	/** @pre None.
+	*   @post The saved list of events is imported.
+	*/
+	Executive();
+	/** @pre None.
+	*   @post The list of events is saved and.
+	*/
+	~Executive();
+	/** @pre none.
+	*   @post will ask which mode and send user to that mode.
+	*   @return none.
+	*/
+	void run();
+	/** @pre none.
+	*   @post User is in admin mode.
+	*   @return none.
+	*/
+	void adminMode();
+	/** @pre none.
+	*   @post User is in attendee mode.
+	*   @return none.
+	*/
+	void attendeeMode();
+};
+
+#include "Executive.cpp"
+#end if

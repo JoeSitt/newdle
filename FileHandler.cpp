2b70f6051075e0e3efa7ee65b88bb6a8e8005b80
Jonathon Greene <jongreene@ku.edu>
2017-09-12 12:24:23 -0500

    Created FileHandler.cpp and created dummy methods.

---
+++ FileHandler.cpp
@@ -0,0 +1,36 @@
+/**
+*	@file FileHandler.cpp
+*	@author Jonathon Greene
+*	@date
+*	@brief
+*/
+
+#include "FileHandler.h"
+
+FileHandler(){
+
+}
+
+FileHandler(char* fileIn){
+
+}
+
+bool checkFile(){
+
+  return true;
+}
+
+bool readFile(){
+
+  return true;
+}
+
+bool saveFile(){
+
+  return true;
+}
+
+void parseXML() const{
+  
+  return true;
+}

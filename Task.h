28260389e31300b932c83e5f3324dd418ed43085
AstralA <AstralA@users.noreply.github.com>
2017-10-04 09:09:45 -0500

    Add files via upload
    Added Task class files.

---
+++ Task.h
@@ -0,0 +1,16 @@
+
+#ifndef TASK_H
+#define TASK_H
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Task
+{
+  public:
+	vector<int> taskAccepted; //when listing attendes and the task they accepted, iterate through the taskList first to show the list of tasks.
+};
+
+#endif
